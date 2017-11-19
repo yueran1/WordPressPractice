7d61ffcaae1353e3d08b8de55006ebec24c254a5
yueran1 <yueran1@ualberta.ca>
2017-11-18 16:08:30 -0800

    add child page menu version

--- learningWordPress/functions.php
+++ learningWordPress/functions.php
@@ -17,7 +17,29 @@ register_nav_menus( array(
     'footer' => __('Footer Menu')
 
     
-) )
+) );
+
+//Get top ancestor
+function get_top_ancestor_id(){
+    global $post;
+
+    if ($post->post_parent){
+        $ancestors=array_reverse(get_post_ancestors( $post->ID ));
+        return $ancestors[0];
+
+
+    }
+
+    return $post->ID;
+}
+
+function have_children(){
+    global $post;
+
+    $pages=get_pages( 'child_of='.$post->ID );
+    return count($pages);
+
+}
 
 
 ?>
--- learningWordPress/page.php
+++ learningWordPress/page.php
@@ -14,6 +14,34 @@ if (have_posts()):
             are two posts        
          -->
          <article class="post page">
+
+            <!-- 
+                here is the child page menu part
+            -->
+            <?php if(have_children() or $post->post_parent >0){   ?>
+                <nav class="site-nav children-links clearfix">
+                    <span class="parent-link"><a href="<?php echo the_permalink(get_top_ancestor_id());?>"><?php echo get_the_title(get_top_ancestor_id());?></a></span>
+                    <ul>
+                        <?php
+                                $args= array(
+                                    //specify the current page been viewed
+                                    //only child of our current page will be showed
+                                    //*******'child_of' => $post->ID,
+
+                                    'child_of' => get_top_ancestor_id(),
+                                    //make list title "page" disapear
+                                    'title_li'=>''
+                                )
+
+                            ?>
+                        <?php wp_list_pages($args); ?>
+                    </ul>
+                 </nav>
+
+            <?php  }  ?>
+            
+
+
             <h2> <?php the_title(); ?></h2>
             <?php the_content();?>
          </article>
