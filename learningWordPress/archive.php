586a2d05c169aaf5c6f4fba901c21b982a54d2de
yueran1 <yueran1@ualberta.ca>
2017-11-21 20:32:43 -0800

    use get_template_part elinimate the duplicate code from index archive and search to keep code dry

--- learningWordPress/archive.php
+++ learningWordPress/archive.php
@@ -40,42 +40,13 @@ if (have_posts()):
     ?></h2>
     <?php
     while (have_posts()): 
-        the_post(); ?>
+        the_post(); 
+
+        get_template_part('content');
     
-        <!--
-            The title will show twice since there
-            are two posts        
-         -->
-         <article class="post">
-            <h2><a href="<?php the_permalink();?>"> <?php the_title(); ?></a></h2>
-
-
-            <!--Output the date for our post and make author name link-->
-            <!--the_time function output the date-->
-            <p class="post-info"><?php the_time('F jS, Y g:i a'); ?> | by <a ><?php the_author_posts_link( )?></a> | Posted in
-            <!-- actually the_category() will do the work-->
-            <?php
-                $categories= get_the_category(  );
-                $separator = ",";
-                $output = '';
-
-                if($categories){
-                    foreach($categories as $category){
-                        //我之前还在想怎么把Output变成 <a>，忘记了Php是可以和html混合使用的
-                        $output .= '<a href="' .get_category_link( $category->term_id ).'">'. $category->cat_name.'</a>' . $separator;
-                    }
-
-                    //trim() will remove the $separator from head and tail
-                    echo trim($output, $separator);
-                }
-            ?>
-            
-            </p>
-            <?php the_excerpt();?>
-         </article>
         
         
-        <?php endwhile;
+         endwhile;
 
         else:
             echo '<p>No Content here</p>';
